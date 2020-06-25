70e5e79a668e9b326db41e67a60d9cf4d7a41cf3
Dr. Lev Avri <avrilev@windowslive.com>
2020-06-25 20:03:45 +0300

    added new config, footer, index & login

---
+++ inc/config.php
@@ -0,0 +1,11 @@
+<?php 
+if(!defined('__CONFIG__')){
+    exit('You do not have a config file.');
+}
+
+define('__ALLOW_FOOTER__', true);
+
+// Our config is below
+
+
+?>
---
+++ inc/footer.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('__ALLOW_FOOTER__')) {
+    exit('You do not have a config file.');
+}
+ ?>
+
+<!-- jquery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit-icons.min.js"></script>
