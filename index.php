4274fdef09c06be466f283b2f7e6c4c856b65edc
Dr. Lev Avri <avrilev@windowslive.com>
2020-06-25 11:14:05 +0300

    adding button to the form

--- index.php
+++ index.php
@@ -30,23 +30,8 @@
                         <input class="uk-input" id="form-stacked-text" type="password" placeholder="password">
                     </div>
                 </div>
-
                 <div class="uk-margin">
-                    <label class="uk-form-label" for="form-stacked-select">Select</label>
-                    <div class="uk-form-controls">
-                        <select class="uk-select" id="form-stacked-select">
-                            <option>Option 01</option>
-                            <option>Option 02</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="uk-margin">
-                    <div class="uk-form-label">Radio</div>
-                    <div class="uk-form-controls">
-                        <label><input class="uk-radio" type="radio" name="radio1"> Option 01</label><br>
-                        <label><input class="uk-radio" type="radio" name="radio1"> Option 02</label>
-                    </div>
+                    <button class="uk-button uk-button-default" type="submit">LogIn</button>
                 </div>
 
             </form>
